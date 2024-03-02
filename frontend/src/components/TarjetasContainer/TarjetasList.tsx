7df7c8bc4e18f766e04d395b2d5c342138148bc6
Adrian Llanes <ados.adrian@gmail.com>
2024-03-02 16:23:49 -0300

    listado de tarjetas inicial agregado

---
+++ frontend/src/components/TarjetasContainer/TarjetasList.tsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import { Tarjeta } from '../../codegen_output'
+import deleteIcon from '../../assets/icons/outline_delete_white_24dp.png'
+import Swal from 'sweetalert2'
+
+interface Props {
+  tarjetasList: Array<Tarjeta>
+  onDeleteTarjeta: (id: number) => void
+}
+
+const keysTabTarjeta = [
+  "RFID",
+  "Activa",
+  "Alta",
+  "Ultimo Uso",
+  "Entregada",
+  "En Salón",
+  "Monto Precargado",
+  "Rol",
+  ""
+]
+
+export const TarjetasList = ({ tarjetasList, onDeleteTarjeta: onDeleteTarjeta_propin }: Props) => {
+
+  const handleDelete = (tarjeta: Tarjeta) => {
+    Swal.fire({
+      title: '¿Estás seguro que deseas eliminar esta tarjeta?',
+      html: `${tarjeta.id} (${tarjeta.rol.nombre_largo})`,
+      showCancelButton: true,
+      confirmButtonText: 'Eliminar',
+      icon: 'warning',
+      confirmButtonColor: '#ff2d55'
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire('Tarjeta eliminada!', '', 'error')
+        onDeleteTarjeta_propin(tarjeta.id)
+      }
+    })
+  }
+
+  return (
+    <>
+    <div className='table-container-xl'>
+      <div className='table-container-l text-center mb-1'>
+        <p className='h3'>Lista de Tarjetas</p>
+      </div>
+      <table className='table table-striped table-hover table-container-l'>
+        <thead className='table-success'>
+          <tr>
+            {keysTabTarjeta.map((item, index) => {
+              return (
+                <th key={index}>{item}</th>
+              )
+            })}
+          </tr>
+        </thead>
+        <tbody className='table-group-divider' >
+          {tarjetasList.map((tarjeta, index) => {
+            return (
+              <tr key={index} >
+                <th scope='row'>{tarjeta.id}</th>
+                <td>{tarjeta.activa ? "Si" : "No"}</td>
+                <td>{tarjeta.fecha_alta}</td>
+                <td>{tarjeta.fecha_ultimo_uso}</td>
+                <td>{tarjeta.entregada ? "Si" : "No"}</td>
+                <td>{tarjeta.presente_en_salon ? "Si" : "No"}</td>
+                <td>{tarjeta.monto_precargado}</td>
+                <td>{tarjeta.rol.nombre_largo}</td>
+                                
+                <td>
+                  <button className='icons-border icon--size icon--delete'
+                    type='button'
+                    onClick={() => { handleDelete(tarjeta) }} >
+                    <img className='icon-img--size' src={deleteIcon} alt="" />
+                  </button>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      </div>
+    </>
+  )
+}
--- frontend/src/components/Views/AdminView.tsx
+++ frontend/src/components/Views/AdminView.tsx
@@ -5,13 +5,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { SideBar } from '../SideBar/SideBar';
 import { ClientsContainer } from '../ClientsContainer/ClientsContainer';
 import { PersonalInternoContainer } from '../PersonalContainer/PersonalContainer';
+import { TarjetasContainer } from '../TarjetasContainer/TarjetasContainer';
 import { categoriesList_sidebar } from '../../types/categoriesList_sidebar';
 import Error404 from './Error404';
-// import { OfficesContainer } from '../OfficesContainer/OfficesContainer';
-// import { DoctorsContainer } from '../DoctorsContainer/DoctorsContainer';
-// import { PatientsContainer } from '../PatientsContainer/PatientsContainer';
-// import { OfficesContainerPatientView } from '../OfficesContainerPatientView/OfficesContainerPatientView';
-// import { DownloadsContainer } from '../DownloadsContainer/DownloadsContainer';
 
 export const AdminView = () => {
   return (
@@ -26,8 +22,8 @@ export const AdminView = () => {
             <Routes>
               <Route path='/' element={<ClientsContainer />} />
               <Route path={categoriesList_sidebar[1].link} element={<PersonalInternoContainer />} />
-              {/* <Route path={categoriesList_sidebar[2].link} element={<PatientsContainer />} />
-              <Route path={categoriesList_sidebar[3].link} element={<DoctorsContainer />} />
+              <Route path={categoriesList_sidebar[2].link} element={<TarjetasContainer />} />
+              {/* <Route path={categoriesList_sidebar[3].link} element={<DoctorsContainer />} />
               <Route path={`${url_waitingRoom}/:query`} element={<OfficesContainerPatientView />} />
               <Route path={categoriesList_sidebar[7].link} element={<DownloadsContainer />} /> */}
               <Route path='*' element={<Error404 />} />
