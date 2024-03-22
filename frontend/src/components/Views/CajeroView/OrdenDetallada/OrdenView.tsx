ad49921afc8e83c4883d094b315e455451e51f24
Adrian Llanes <ados.adrian@gmail.com>
2024-03-22 14:27:26 -0300

    pedidos bajo accordion y estilos actualizados

--- frontend/src/components/Views/CajeroView/OrdenDetallada/OrdenView.tsx
+++ frontend/src/components/Views/CajeroView/OrdenDetallada/OrdenView.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row } from 'react-bootstrap';
+import { Row, Accordion } from 'react-bootstrap';
 import { OrdenCompraCerrada } from '../../../../codegen_output';
 import PedidosList from './PedidosList';
 import OrdenMetadata from './OrdenMetadata';
@@ -10,12 +10,19 @@ interface OrdenViewProps {
 
 const OrdenView: React.FC<OrdenViewProps> = ({ ordenData }) => {
   return (
-    <div>
+    <div className="orden-view">
       <Row>
         <OrdenMetadata ordenData={ordenData} />
       </Row>
       <Row>
-        <PedidosList pedidos={ordenData.pedidos} />
+        <Accordion defaultActiveKey="" className="pedidos-accordion">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Pedidos</Accordion.Header>
+            <Accordion.Body>
+              <PedidosList pedidos={ordenData.pedidos} />
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
       </Row>
     </div>
   );
