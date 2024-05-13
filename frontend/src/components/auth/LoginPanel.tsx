1c8dd91c5082762c555f788de2e58738600d5ea3
Adrian Llanes <ados.adrian@gmail.com>
2024-05-13 00:17:27 -0300

    mejorado login panel

--- frontend/src/components/auth/LoginPanel.tsx
+++ frontend/src/components/auth/LoginPanel.tsx
@@ -1,26 +1,42 @@
-import React, { useState } from 'react';
-import CardReaderModal from '../ClientsContainer/CardReaderModal';
+import React, { useState, useEffect } from 'react';
+import { Modal, Spinner, Row, Col } from 'react-bootstrap';
 import { useAuth } from './AuthContext';
+import { LockFill } from 'react-bootstrap-icons';
 
-interface LoginPanelProps {  
-}
+const LoginPanel = () => {
+  const [cardNumber, setCardNumber] = useState('');
+  const { login } = useAuth();
 
-const LoginPanel: React.FC<LoginPanelProps> = () => {
-  const [showCardReader, setShowCardReader] = useState(true);
-  const { isLoggedIn, user, login, logout } = useAuth();
-  
-  const handleCardRead = async (cardId: string) => {
-    // setRfid(cardId); // This will trigger the useEffect above
-    await login(cardId)
+  const handleKeyDown = (event: any) => {
+    if (event.key === 'Enter' && cardNumber) {
+      login(cardNumber);
+      setCardNumber(''); // Reset after attempt
+    } else if (/\d/.test(event.key)) {
+      setCardNumber(cardNumber + event.key);
+    }
   };
 
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [cardNumber]);
+
   return (
-    <CardReaderModal 
-        show={showCardReader} 
-        onHide={() => setShowCardReader(true)} 
-        onCardRead={handleCardRead} 
-        title="Login RFID" 
-    />
+    <Modal show={true} onHide={() => {}} centered className="login-modal">
+      <Modal.Header closeButton>
+        <Modal.Title>
+          <LockFill  size={50} className='me-3'/> 
+          Login RFID
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="text-center login-logo-body">
+        <p>Acerque la tarjeta al lector</p>
+        <Spinner animation="border" variant="light" />
+        <p>Número: {cardNumber}</p>
+      </Modal.Body>
+    </Modal>
   );
 };
 
