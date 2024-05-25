2318562ad8add95c139f8241f61a33b79430c7f4
Adrian Llanes <ados.adrian@gmail.com>
2024-05-25 19:07:58 -0300

    agregado panel de busqueda en vista de cajero

--- frontend/src/components/Views/CajeroView/BusquedaPanel/BusquedaPanel.tsx
+++ frontend/src/components/Views/CajeroView/BusquedaPanel/BusquedaPanel.tsx
@@ -1,62 +1,71 @@
 // BusquedaPanel.tsx
 import React, { useState } from 'react';
-import { Button, Modal, Card } from 'react-bootstrap';
+import { Button, Modal, Card, Form, Row, Col } from 'react-bootstrap';
 import { handleApiError } from '../../../ClientsContainer/ClientsContainer';
-import { ClientesService, ClienteCreate, OrdenesService, OrdenCompraDetallada } from '../../../../codegen_output';
-import Swal from 'sweetalert2';
-import copaImage from '../../../../assets/icons/copa.png'
+import { OrdenesService, OrdenCompraDetallada } from '../../../../codegen_output';
 import { OrdenesList } from '../../../OrdenesContainer/OrdenesListCard';
+import { Search } from 'react-bootstrap-icons';
 
 const BusquedaPanel = () => {
   const [showModal, setShowModal] = useState(false);
   const [matchingOrdenes, setMatchingOrders] = useState<OrdenCompraDetallada[]>([]);
-  const [showPanelBusqueda, setShowPanelBusqueda] = useState(false);
+  const [clientName, setClientName] = useState('');
 
   const handleShow = () => setShowModal(true);
   const handleClose = () => setShowModal(false);
 
-  const handleSearchClient = async (clientName: string): Promise<void> => {
-      
+  const handleSearchClient = async (): Promise<void> => {
     try {
-        const response = await OrdenesService.handleReadOrdenAbiertaByClientNameBackendApiV1OrdenesByNameClientNameGet(clientName);
-        setMatchingOrders(response)
+      const response = await OrdenesService.handleReadOrdenAbiertaByClientNameBackendApiV1OrdenesByNameClientNameGet(clientName);
+      setMatchingOrders(response);
+      setShowModal(true); // Show the modal with the results after fetching
     } catch (error) {
-        handleApiError(error)
+      handleApiError(error);      
     }
   };
 
-  const handleClosePanelCobro = () => {
-    setShowPanelBusqueda(false);
-  };
-
-  const handleAbrirPanelCobro = () => {
-    setShowPanelBusqueda(true)
-  }
-
   return (
     <Card className='transparent-card acciones-width'>
-        <Card.Header>Buscar Cliente</Card.Header>
-        <Card.Body>
-            <div className="accionable" >
-            <Button 
-                className="accionable-button" // Apply the custom class
-                onClick={handleShow}
-            >
-                    <div className="accionable--contenido">
-                        <img src={copaImage} alt="Copa Icon" className="icon" />
-                        <h3>Buscar Cliente</h3>
-                    </div>
-                </Button>
-            </div>            
-        </Card.Body>
-    
-        <Modal>
-            <OrdenesList
-                ordenesList={matchingOrdenes}
-                onDeleteOrden={() => {}}
-                columnasReducidas={true}
-            />
-        </Modal>
+      <Card.Header>Buscar Cliente</Card.Header>
+      <Card.Body>
+        <Form>
+            <Row >
+                <Col>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Control
+                        type="text"
+                        placeholder="Ingrese un nombre..."
+                        value={clientName}
+                        onChange={e => setClientName(e.target.value)}
+                        />
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Button variant="secondary" onClick={handleSearchClient} className='p-2'>
+                        <Search size={20} className="" />
+                    </Button>
+                </Col>
+            </Row>
+        </Form>
+      </Card.Body>
+
+      <Modal show={showModal} onHide={handleClose} size='lg'>
+        <Modal.Header closeButton>
+          <Modal.Title>Ordenes Encontradas</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <OrdenesList
+            ordenesList={matchingOrdenes}
+            onDeleteOrden={() => {}}
+            columnasReducidas={true}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cerrar
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Card>
   );
 };
